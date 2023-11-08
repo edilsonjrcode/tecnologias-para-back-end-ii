47babd9dc8198849d681b4376fa8af7f2e1b46dc
edilsonjrcode <2022111510116@iesp.edu.br>
2023-11-07 23:44:45 -0300

    first commit

--- projeto-segunda-unidade/atividade-01/src/main/java/com/projetosegundaunidade/projetosegundaunidade/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projetosegundaunidade.projetosegundaunidade.repository;
-
-import com.projetosegundaunidade.projetosegundaunidade.entity.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
